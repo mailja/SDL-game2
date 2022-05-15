9ad4067e7d8d642648498e6ba2b21fd1ba55b52c
Mailja <markus.iljas@gmail.com>
2022-05-16 00:10:37 +0200

    Initial effortAdd project files.

---
+++ SDL-game2/Game.h
@@ -0,0 +1,31 @@
+#pragma once
+#include <SDL.h>
+
+#ifndef __Game__
+#define __Game__
+
+class Game
+{
+public:
+   Game() {}
+   ~Game() {}
+   // simply set the running variable to true
+   void init() { m_bRunning = true; }
+   void render();
+   void update() {}
+   void handleEvents();
+   void clean();
+   // a function to access the private running variable
+   bool running() { return m_bRunning; }
+
+   bool init(const char* title, int xpos, int ypos, int width, int
+      height, int flags);
+private:
+   SDL_Window* m_pWindow = 0;
+   SDL_Renderer* m_pRenderer = 0;
+
+   bool m_bRunning;
+};
+
+#endif /* defined(__Game__) */
+
---
+++ SDL-game2/main.cpp
@@ -0,0 +1,56 @@
+#include <cstdio>
+#include<SDL.h>
+#include "Game.h"
+
+// our Game object
+Game* g_game = 0;
+
+SDL_Window* g_pWindow = 0;
+SDL_Renderer* g_pRenderer = 0;
+
+bool g_bRunning = false; // this will create a loop
+
+//void render()
+//{
+//   // set to black
+//   SDL_SetRenderDrawColor(g_pRenderer, 0, 0, 0, 255);
+//   // clear the window to black
+//   SDL_RenderClear(g_pRenderer);
+//   // show the window
+//   SDL_RenderPresent(g_pRenderer);
+//}
+
+
+int main(int argc, char* args[])
+{
+
+   g_game = new Game();
+   g_game->init("Chapter 1", 100, 100, 640, 480, 0);
+   while (g_game->running())
+   {
+      g_game->handleEvents();
+      g_game->update();
+      g_game->render();
+   }
+   g_game->clean();
+   return 0;
+
+   //if (init("Chapter 1: Setting up SDL",
+   //   SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640,
+   //   480, SDL_WINDOW_SHOWN))
+   //{
+   //   g_bRunning = true;
+   //}
+   //else
+   //{
+   //   return 1; // something's wrong
+   //}
+
+   //while (g_bRunning)
+   //{
+   //   render();
+   //}
+   //// clean up SDL
+   //SDL_Quit();
+   //return 0;
+}
